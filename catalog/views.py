4c038984b09593cbf821dbd7296dd4a1d2fe1e1d
yuzhakov <yuzhakov.a.a@mail.ru>
2026-05-25 21:19:44 +0500

    домашка 26.1 на сдачу 01

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
@@ -23,13 +24,13 @@ class ProductDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ("name", "description", "picture", "category", "price", "created_at", "updated_at")
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:catalog_list' )
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ("name", "description", "picture", "category", "price", "created_at", "updated_at")
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:catalog_list' )
 
     def get_success_url(self):
